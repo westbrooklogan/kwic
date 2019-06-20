7f8e14d4faa0c5e1329e9379b205f5bfb2870f58
Logan Westbrook <westbrooklogan@gmail.com>
2019-06-20 03:16:37 -0500

    server update

--- kwic/server.ts
+++ kwic/server.ts
@@ -8,10 +8,16 @@ import { apiKwic as Kwic } from "./api/apiKwic/apiKwic";
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 import * as bodyparser from "body-parser";
 const jsonParser = bodyparser.json();
 
 app.post("/KWIC", jsonParser, parser, lineStorage, cyclicShifter, combiner, alphabetizer);
 
 // start server and listen to incoming request
-app.listen(process.env.PORT || 8091, () => {console.log("Server started...")});
+app.listen(process.env.PORT || 8080, () => {console.log("Server started...")});
