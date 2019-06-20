6cb3e11f8a3bf1346acf5cac746701c1e382c965
David Martinez <david.mtz00@hotmail.com>
2019-06-20 04:04:48 -0500

    Updates

---
+++ precache-manifest.0e71862ba9d20d6ea68403221504642f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc3b8d1eea1ce4ce30b6cafeb6d4ac6",
+    "url": "/kwic/index.html"
+  },
+  {
+    "revision": "e9c62e1e417832e51185",
+    "url": "/kwic/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "58c61cfe149a4a73f37f",
+    "url": "/kwic/static/js/2.dd66a7cf.chunk.js"
+  },
+  {
+    "revision": "e9c62e1e417832e51185",
+    "url": "/kwic/static/js/main.8794880b.chunk.js"
+  },
+  {
+    "revision": "b78d9338bc6a81fbdc2e",
+    "url": "/kwic/static/js/runtime~main.ecbf97cf.js"
+  }
+]);
